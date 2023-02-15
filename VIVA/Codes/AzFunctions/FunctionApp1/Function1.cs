13cfb842fb63a92e05e86a8ecdfd82a54491a97c
Preeti <prteotia@microsoft.com>
2023-02-15 09:25:34 +0530

    Function app to return request headers

---
+++ VIVA/Codes/AzFunctions/FunctionApp1/Function1.cs
@@ -0,0 +1,47 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FunctionApp1
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+            if (req.Headers != null)
+            {
+                IHeaderDictionary headers = req.Headers;
+                string output = "";
+                foreach (var kv in headers)
+                {
+                    output = output + kv.Key + "=" + kv.Value + "\n";
+                }
+                Console.WriteLine(output);
+                return new JsonResult(output);
+            }
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+           // return new OkObjectResult(responseMessage);
+            return new JsonResult(responseMessage);
+        }
+    }
+}
